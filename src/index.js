cc016fbf8463059564e70f56e9e5f76f43cc5f70
lin <lin@lindeAir-2.lan>
2019-03-08 21:26:29 +0800

    无修改， 注释一段代码

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// function createObject() {
+//   console.log('outermost this', this)
+
+//   return {
+//     arrowFunction: () => {
+//       console.log('allowFunction this', this)
+//     },
+//     functionKeywordFunction: function() {
+//       console.log('functionKeyword this', this)
+//     }
+//   }
+// }
+
+// const obj = createObject()
+// console.log('obj', obj)
+// obj.arrowFunction()
+// obj.functionKeywordFunction()
