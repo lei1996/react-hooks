83288e5ad1fcb730416a71a77d7cb877d831ad21
lin <lin@lindeMacBook-Air-2.local>
2019-03-08 17:22:54 +0800

    输入框 跳转 百度

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import React, { useState } from 'react'
 function App() {
   const [userQuery, setUserQuery] = useState('')
 
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery)
+    setUserQuery(event.target.value)
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery()
+    }
+  }
+
+  const searchQuery = () => {
+    window.open(`https://www.baidu.com/s?wd=${userQuery}`)
+  }
+
   return (
     <div className="App">
       <h1>Hello David</h1>
+      <div className="form">
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+        <button onClick={searchQuery}>搜索</button>
+      </div>
     </div>
   )
 }
