8973244458f69f2856ce008378a90e18265d4607
lin <lin@lindeAir-2.lan>
2019-03-10 19:25:39 +0800

    Updates

---
+++ precache-manifest.8ac8e29667c461a06b2763114120a89e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "90221fbdcf8dc3f45c1f",
+    "url": "/static/js/main.212a4f91.chunk.js"
+  },
+  {
+    "revision": "8d287b2727d10525a115",
+    "url": "/static/js/2.8519b4ce.chunk.js"
+  },
+  {
+    "revision": "90221fbdcf8dc3f45c1f",
+    "url": "/static/css/main.44ea15dc.chunk.css"
+  },
+  {
+    "revision": "ce45d73a1c20265c03b754ab278d82eb",
+    "url": "/index.html"
+  }
+];
