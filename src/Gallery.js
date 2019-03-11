032489859a5e96180383a5ac4bf3a0e8537a4a2c
lin <lin@lindeAir-2.lan>
2019-03-11 15:03:03 +0800

    add useDynamicTransition common function.

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,33 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import PICTURES from './data/pictures'
+import { useDynamicTransition } from './hooks'
 
 const SECONDS = 1000
 const minimumDelay = 1 * SECONDS
 const minimumIncrement = 1
 
 function Gallery() {
-  const [index, setIndex] = useState(0)
   const [delay, setDelay] = useState(3 * SECONDS)
   const [increment, setIncrement] = useState(1)
 
-  useEffect(() => {
-    console.log('delay', delay, 'increment', increment)
-
-    const interval = setInterval(() => {
-      setIndex(storedIndex => {
-        // 这里storedIndex 拿到的是 index 的值
-        console.log('storedIndex', storedIndex)
-        return (storedIndex + increment) % PICTURES.length
-      })
-      //   console.log('(index + 1) % PICTURES.length', (index + 1) % PICTURES.length)
-    }, delay)
-
-    return () => {
-      // 在移除组件的时候，移除定时器，避免内存泄漏
-      clearInterval(interval)
-    }
-    // [delay, increment] 注册 使里面的参数可以变更检测
-  }, [delay, increment])
+  const index = useDynamicTransition({
+    delay,
+    increment,
+    length: PICTURES.length
+  })
 
   const updateDelay = event => {
     const delay = Number(event.target.value) * SECONDS
--- src/hooks.js
+++ src/hooks.js
@@ -11,3 +11,25 @@ export const useFetch = (url, initialValue) => {
 
   return result
 }
+
+export const useDynamicTransition = ({ increment, delay, length }) => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(storedIndex => {
+        // 这里storedIndex 拿到的是 index 的值
+        console.log('storedIndex', storedIndex)
+        return (storedIndex + increment) % length
+      })
+      //   console.log('(index + 1) % PICTURES.length', (index + 1) % PICTURES.length)
+    }, delay)
+
+    // 在移除组件的时候，移除定时器，避免内存泄漏
+    return () => clearInterval(interval)
+
+    // [delay, increment] 注册 使里面的参数可以变更检测
+  }, [delay, increment])
+
+  return index
+}
